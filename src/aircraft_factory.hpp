4b6e8c88ea6fe7622d6370b1a584f3defd3457b6
Cyril VAN <cvan1@etud.u-pem.fr>
2022-02-26 13:45:13 +0100

    Task 1 | Objectif 2 | A

--- src/aircraft_factory.hpp
+++ src/aircraft_factory.hpp
@@ -1,9 +1,27 @@
 #pragma once
 
+#include "aircraft.hpp"
+#include "airport.hpp"
+
+struct AircraftType;
+
 class AircraftFactory
 {
-public:
-    AircraftFactory() {}
+private:
+    const std::string airlines[8]          = { "AF", "LH", "EY", "DL", "KL", "BA", "AY", "EY" };
+    const static size_t NUM_AIRCRAFT_TYPES = 3;
+    AircraftType* aircraft_types[NUM_AIRCRAFT_TYPES] {};
 
-    void create_aircraft();
+public:
+    AircraftFactory()
+    {
+        {
+            aircraft_types[0] = new AircraftType { .02f, .05f, .02f, MediaPath { "l1011_48px.png" } };
+            aircraft_types[1] = new AircraftType { .02f, .05f, .02f, MediaPath { "b707_jat.png" } };
+            aircraft_types[2] = new AircraftType { .02f, .1f, .04f, MediaPath { "concorde_af.png" } };
+        }
+    }
+
+    Aircraft* create_aircraft(Airport* airport, const AircraftType& type);
+    Aircraft* create_random_aircraft(Airport* airport);
 };
--- src/tower_sim.hpp
+++ src/tower_sim.hpp
@@ -1,24 +1,40 @@
 #pragma once
 
-#include "aircraft_factory.hpp"
+#include "GL/opengl_interface.hpp"
+#include "config.hpp"
+
+#include <cstdlib>
+#include <ctime>
+
+struct ContextInitializer
+{
+public:
+    ContextInitializer(int argc, char** argv)
+    {
+        MediaPath::initialize(argv[0]);
+        std::srand(static_cast<unsigned int>(std::time(nullptr)));
+        GL::init_gl(argc, argv, "Airport Tower Simulation");
+    }
+};
 
 class Airport;
-struct AircraftType;
 class AircraftManager;
+class AircraftFactory;
 
 class TowerSimulation
 {
 private:
-    bool help                         = false;
+    bool help = false;
+    ContextInitializer initializer;
+    AircraftFactory* aircraft_factory;
+
     Airport* airport                  = nullptr;
     AircraftManager* aircraft_manager = nullptr;
-    AircraftFactory aircraft_factory  = AircraftFactory();
 
     TowerSimulation(const TowerSimulation&) = delete;
     TowerSimulation& operator=(const TowerSimulation&) = delete;
 
-    void create_aircraft(const AircraftType& type) const;
-    void create_random_aircraft() const;
+    void create_aircraft() const;
 
     void create_keystrokes() const;
     void display_help() const;
