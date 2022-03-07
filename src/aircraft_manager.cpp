0da74ffc75e7588363e267d2f108ab92335c1b4a
Cyril VAN <cvan1@etud.u-pem.fr>
2022-03-07 20:55:12 +0100

    Task 2 | Objectif 2 | C

--- src/aircraft_manager.cpp
+++ src/aircraft_manager.cpp
@@ -5,6 +5,27 @@
 
 void AircraftManager::move()
 {
+    std::sort(aircrafts.begin(), aircrafts.end(),
+              [](std::unique_ptr<Aircraft>& first, std::unique_ptr<Aircraft>& second)
+              {
+                  if (first->has_terminal() && second->has_terminal())
+                  {
+                      return first->remaining_fuel() < second->remaining_fuel();
+                  }
+
+                  if (first->has_terminal())
+                  {
+                      return true;
+                  }
+
+                  if (second->has_terminal())
+                  {
+                      return false;
+                  }
+
+                  return first->remaining_fuel() < second->remaining_fuel();
+              });
+
     aircrafts.erase(std::remove_if(aircrafts.begin(), aircrafts.end(),
                                    [](std::unique_ptr<Aircraft>& aircraft)
                                    { return !aircraft->move() || aircraft->remaining_fuel() == 0; }),
