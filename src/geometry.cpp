47b2c418dd0cfbaa0b51d8c6d0f570925e5cf293
Cyril VAN <cvan1@etud.u-pem.fr>
2022-04-06 13:45:47 +0200

    Task 4 | Objectif 2 - 3 | Create gemoetry cpp

---
+++ src/geometry.cpp
@@ -0,0 +1,22 @@
+#include "geometry.hpp"
+
+void test_generic_points()
+{
+    Point<3, int> p1;
+    Point<3, int> p2;
+    auto p3 = p1 + p2;
+    p1 += p3;
+    p1 *= 3; // ou 3.f, ou 3.0 en fonction du type de Point
+
+    Point<3, float> p4;
+    Point<3, float> p5;
+    auto p6 = p4 + p5;
+    p4 += p6;
+    p4 *= 3.f; // ou 3.f, ou 3.0 en fonction du type de Point
+
+    Point<2, double> p7;
+    Point<2, double> p8;
+    auto p9 = p7 + p8;
+    p7 += p9;
+    p7 *= 3.0; // ou 3.f, ou 3.0 en fonction du type de Point
+}
